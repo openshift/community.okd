0778a3fabbc4ddf143d8c8e045d03028321a03d4
Adam Miller <admiller@redhat.com>
2020-11-16 22:28:43 -0600

    remove elements of the json dump that aren't valid for DOCUMENTATION
    Signed-off-by: Adam Miller <admiller@redhat.com>

--- ci/downstream_fragments.py
+++ ci/downstream_fragments.py
@@ -7,6 +7,10 @@ import sys
 with open("./rendereddocfragments.txt", 'w') as df_fd:
     with open(sys.argv[2], 'r') as fd:
         json_docs = json.load(fd)
+
+        json_docs[sys.argv[1]]['doc'].pop('collection')
+        json_docs[sys.argv[1]]['doc'].pop('filename')
+
         df_fd.write("DOCUMENTATION = '''\n")
         df_fd.write(yaml.dump(json_docs[sys.argv[1]]['doc']))
         df_fd.write("'''\n\n")
