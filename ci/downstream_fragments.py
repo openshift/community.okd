c0ec91e9c39ee6a439f008d65229fd241dd3e9d2
abikouo <abikouo@redhat.com>
2022-04-28 16:14:15 +0200

    fix header generation

--- ci/downstream_fragments.py
+++ ci/downstream_fragments.py
@@ -15,14 +15,14 @@ with open("./rendereddocfragments.txt", 'w') as df_fd:
         json_docs[sys.argv[1]]['doc'].pop('filename', '')
         json_docs[sys.argv[1]]['doc'].pop('has_action', '')
 
-        df_fd.write("DOCUMENTATION = '''\n")
+        df_fd.write('DOCUMENTATION = """\n')
         df_fd.write(yaml.dump(json_docs[sys.argv[1]]['doc'], default_flow_style=False))
-        df_fd.write("'''\n\n")
+        df_fd.write('"""\n\n')
 
-        df_fd.write("EXAMPLES = '''\n")
+        df_fd.write('EXAMPLES = """')
         df_fd.write(json_docs[sys.argv[1]]['examples'])
-        df_fd.write("'''\n\n")
+        df_fd.write('"""\n\n')
 
-        df_fd.write("RETURN = '''\n")
-        df_fd.write(yaml.dump(json_docs[sys.argv[1]]['return'], default_flow_style=False))
-        df_fd.write("'''\n\n")
+        df_fd.write('RETURN = r"""')
+        df_fd.write(json_docs[sys.argv[1]]['return'])
+        df_fd.write('"""\n\n')
