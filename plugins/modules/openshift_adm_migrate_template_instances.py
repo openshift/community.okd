bf5a4aa91d954ce4ceca7b4951716e9be79abfbe
abikouo <abikouo@redhat.com>
2023-01-25 15:42:03 +0100

    fix downstream sanity

--- plugins/modules/openshift_adm_migrate_template_instances.py
+++ plugins/modules/openshift_adm_migrate_template_instances.py
@@ -233,7 +233,6 @@ result:
 """
 # ENDREMOVE (downstream)
 
-
 from ansible.module_utils._text import to_native
 
 from ansible_collections.community.okd.plugins.module_utils.openshift_common import AnsibleOpenshiftModule
